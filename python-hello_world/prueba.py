cda7a64b8ca7d3a1c53ea4ed60978477077e59be
ZALOFARG <andre.work.go@gmail.com>
2023-11-07 17:27:27 -0800

    1er. commit t7

---
+++ python-hello_world/prueba.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "este es un refran muy largo\
+        que me llega al pincho escribir"
+print("este es un refran muy largo que me llega al pincho escribir")
+print(len(str))
