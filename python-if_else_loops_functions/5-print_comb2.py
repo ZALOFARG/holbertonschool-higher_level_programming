efc08409f73f1f4ebd49aaf4d88f98382dccdd05
ZALOFARG <andre.work.go@gmail.com>
2023-11-08 15:38:47 -0800

    arreglo del numero de prints

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i == 99:
-        print("{:02}".format(i), end="")
+        print("{:02}".format(i), end="\n")
         break
     print("{:02}, ".format(i), end="")
-print()
