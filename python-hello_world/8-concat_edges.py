1b6925ffdfbece2fd5f1b657dc5e93ce5c68b899
ZALOFARG <andre.work.go@gmail.com>
2023-11-08 11:25:28 -0800

    t6 añadiendo linea nueva

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
          language that combines remarkable power with very clear syntax"
-print(str[39:67] + str[115:120] + str[:7])
+print(str[39:67] + str[115:120] + str[:6] + "\n")
 
