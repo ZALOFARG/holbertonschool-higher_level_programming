56b69be4f1e9cae6956f17867457b94d1f4aee43
ZALOFARG <andre.work.go@gmail.com>
2023-11-08 11:21:14 -0800

    usando slices para t6

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
          language that combines remarkable power with very clear syntax"
-str = str.replace("Python is an interpreted, interactive, object-oriented programming\
-         language that combines remarkable power with very clear syntax", "object-oriented programming with Python")
-print(str)
+print(str[39:67] + str[115:120] + str[:6])
+
