77a78799db5af8298a6b1ea7e8a0997158c9c5da
ZALOFARG <andre.work.go@gmail.com>
2023-11-09 18:38:04 -0800

    commit utilizando ternario y requerimeintos

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
     for x in str:
-        if ((ord(x)) == 32) or (ord(x) in range(48, 58)):
-            print(chr(ord(x)), end="")
-        else:
-            print(chr(ord(x) - 32), end="\n")
+        print(x if (ord(x) == 32) or (ord(x) in range(48, 58)) or (ord(x) in range(65, 91)) else (chr(ord(x) - 32)), end="")
+    print()
