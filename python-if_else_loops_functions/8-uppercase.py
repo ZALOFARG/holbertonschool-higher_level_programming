07f0e5a18128f146bd389b44b3095853f77aa2cc
ZALOFARG <andre.work.go@gmail.com>
2023-11-09 18:40:26 -0800

    commit usando .format

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
     for x in str:
-        print(x if (ord(x) == 32) or (ord(x) in range(48, 58)) or (ord(x) in range(65, 91)) else (chr(ord(x) - 32)), end="")
+        print("{}".format(x) if (ord(x) == 32) or (ord(x) in range(48, 58)) or (ord(x) in range(65, 91)) else "{}".format((chr(ord(x) - 32))), end="")
     print()
