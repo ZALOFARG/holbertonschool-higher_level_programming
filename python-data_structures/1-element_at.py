64c382a501220c69bc06ffde3da25758611b22c7
ZALOFARG <andre.work.go@gmail.com>
2023-11-11 12:22:39 -0800

    commit correct else in if/else

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,9 +3,7 @@ def element_at(my_list, idx):
     if idx >= len(my_list) or idx < 0:
         return None
     else:
-        for i in my_list:
-            if i == idx:
-                return my_list[i]
+        return my_list[idx]
 
 
 if __name__ == '__main__':
