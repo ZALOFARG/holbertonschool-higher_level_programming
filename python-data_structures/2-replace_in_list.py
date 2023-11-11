e374e0bb5f435f98ce3437d81d41b9b594ab9ac9
ZALOFARG <andre.work.go@gmail.com>
2023-11-11 12:45:09 -0800

    1er. commit t2

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        my_list.remove(my_list[idx])
+        my_list.insert(idx, element)
+        return my_list
+
+
+if __name__ == '__main__':
+    replace_in_list(my_list, idx, element)
