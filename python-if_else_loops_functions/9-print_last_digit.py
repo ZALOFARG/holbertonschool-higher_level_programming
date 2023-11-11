2ee842ae7cd2f2af050c6c58273b1d34afbb35d9
ZALOFARG <andre.work.go@gmail.com>
2023-11-11 10:26:52 -0800

    adding the style

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def print_last_digit(number):
     if number < 0:
         number *= -1
@@ -6,6 +7,7 @@ def print_last_digit(number):
     else:
         print(number % 10, end="")
     return number % 10
+
 
 if __name__ == '__main__':
     print_last_digit()
