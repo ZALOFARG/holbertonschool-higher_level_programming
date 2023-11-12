708d42659d0eda5f41f23994e95d197bc866ec84
ZALOFARG <andre.work.go@gmail.com>
2023-11-12 15:47:34 -0800

    1er. commit t8

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    ext = len(sentence)
+    primer = sentence[0]
+    return ext, primer
+
+
+if __name__ == '__main__':
+    multiple_returns(sentence)
