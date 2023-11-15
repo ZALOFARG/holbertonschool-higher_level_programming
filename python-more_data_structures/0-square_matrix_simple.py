36108bf8435f5d1d97868402335987acaf68c72b
ZALOFARG <andre.work.go@gmail.com>
2023-11-14 18:40:43 -0800

    1st commit t0

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    b = []
+    for i in range(len(matrix)):
+        b_row = []
+        for j in range(3):
+            b_row.append(matrix[i][j] ** 2)
+        b.append(b_row)
+    return b
+
+
+if __name__ == '__main__':
+    square_matrix_simple(matrix=[])
