b8f75d2b19406e42739c62105f560208eb415d84
ZALOFARG <andre.work.go@gmail.com>
2023-11-15 17:55:18 -0800

    1st. commit t7

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary.update({key: value})
+    return a_dictionary
+
+
+if __name__ == '__main__':
+    update_dictionary(a_dictionary, key, value)
