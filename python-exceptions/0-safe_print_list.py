52b733ad6605bba27bf2a6267908ba5e21c5f153
ZALOFARG <andre.work.go@gmail.com>
2023-11-17 12:59:59 -0800

    using requirements

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,14 +2,16 @@
 def safe_print_list(my_list=[], x=0):
     try:
         i = 0
+        cont = 0
         while i < x:
             print(my_list[i], end="")
             i += 1
+            cont += 1
         print()
         return x
     except IndexError:
         print()
-        return len(my_list)
+        return cont
 
 
 if __name__ == '__main__':
