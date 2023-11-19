27af4ddd23cd37233a0159ef23ee38cc4d5ea048
ZALOFARG <andre.work.go@gmail.com>
2023-11-19 15:36:09 -0800

    commit trying to explain more

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 class Square:
-    """Creates and defines a class"""
-    pass
+    """Creates and defines a class.
 
+    do <obj> = class()
+    get and instance of the class.
+    """
+    pass
