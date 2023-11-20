d224dba27c7833bfed38864f742e3ac54f4c7e93
ZALOFARG <andre.work.go@gmail.com>
2023-11-19 20:16:57 -0800

    1st. commit t3

---
+++ python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This module will manage classes"""
+
+
+class Square:
+    """Creates and defines a class.
+
+    do <obj> = class()
+    get and instance of the class.
+    """
+    def __init__(self, size=0):
+        """construct method
+
+        Args:
+            self: the object is being created.
+            size: size of the square being created
+        """
+        self.__size = size
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """public methos that returns the area of the square"""
+        return self.__size ** 2
