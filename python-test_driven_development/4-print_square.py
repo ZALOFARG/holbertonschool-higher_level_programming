47c8472ff9727084fcfbd357532f2e687e224c1a
ZALOFARG <andre.work.go@gmail.com>
2023-11-22 20:45:57 -0800

    1st commit t3

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+def print_square(size):
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) and (size < 0):
+        raise TypeError("size must be an integer")
+    if size == 0:
+        print()
+    for i in range(size):
+        for j in range(size):
+            print('#', end="")
+        print()
+
+
+if __name__ == '__main__':
+    print_square(size)
