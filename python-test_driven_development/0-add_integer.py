afbccca5df95db304c76b99cb63967e62b8cc7ca
ZALOFARG <andre.work.go@gmail.com>
2023-11-22 17:21:57 -0800

    1st commit t0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module containing the different math expretions"""
+
+
+def add_integer(a, b=98):
+    """
+    function that after validating the inputs returns a sum
+
+    Args:
+        a: operand 1
+        b: operand 2
+
+    Returns the sum
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a + b)
+
+
+if __name__ == '__main__':
+    add_integer(a, b=98)
