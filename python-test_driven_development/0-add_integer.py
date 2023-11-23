ffed1708fcfdc5e4658f3a0713f993c5d0266693
ZALOFARG <andre.work.go@gmail.com>
2023-11-22 17:52:12 -0800

    commit with additional test cases

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -16,6 +16,9 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
+    result = a + b
+    if result == float('inf') or result == -float('inf'):
+        return 98
     return int(a + b)
 
 
