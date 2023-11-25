8d1bf0a066a1be72abf1a0ca2ce8d666d847708d
ZALOFARG <andre.work.go@gmail.com>
2023-11-24 16:10:38 -0800

    adding finally sentence

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,19 +3,21 @@ def list_division(my_list_1, my_list_2, list_length):
     my_list_3 = []
     for i in range(list_length):
         try:
-            my_list_3.append(my_list_1[i] / my_list_2[i])
+            res = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("division by 0")
-            my_list_3.append(0)
+            res = 0
             continue
         except TypeError:
             print("wrong type")
-            my_list_3.append(0)
+            rest = 0
             continue
         except IndexError:
             print("out of range")
-            my_list_3.append(0)
+            rest = 0
             continue
+        finally:
+            my_list_3.append(res)
     return my_list_3
 
 
