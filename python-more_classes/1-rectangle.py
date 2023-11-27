d3229ce728989e171524833373b2a28cf2990473
ZALOFARG <andre.work.go@gmail.com>
2023-11-27 07:19:00 -0800

    another try using decorators

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,31 +6,33 @@ class Rectangle:
     """class defining a rectangle"""
     def __init__(self, width=0, height=0):
         """constructor method to initialize instances"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
+    @property
     def width(self):
         """getter width method"""
         return self.__width
 
+    @width.setter
     def width(self, value):
         """setter width method"""
         if type(value) != int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
+    @property
     def height(self):
         """getter height method"""
         return self.__height
 
+    @height.setter
     def height(self, value):
         """setter height method"""
         if type(value) != int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
