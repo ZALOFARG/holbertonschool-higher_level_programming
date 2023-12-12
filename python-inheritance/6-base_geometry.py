4454c42d41e10189e7b60b9d6343b769aa5d10ec
ZALOFARG <andre.work.go@gmail.com>
2023-12-12 04:39:04 -0800

    1st commit t6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this module will define a class then work with it"""
+
+
+class BaseGeometry:
+    """working with a class"""
+    def area(self):
+        """method that raises an excep"""
+        raise Exception("area() is not implemented")
