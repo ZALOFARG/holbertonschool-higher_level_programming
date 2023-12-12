308558362d08685a4c950d869c65e452c41e311a
ZALOFARG <andre.work.go@gmail.com>
2023-12-12 05:09:59 -0800

    1st commit t7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""this module will define a class then work with it"""
+
+
+class BaseGeometry:
+    """working with a class"""
+    def area(self):
+        """method that raises an excep"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """func that vverifies value argument"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
