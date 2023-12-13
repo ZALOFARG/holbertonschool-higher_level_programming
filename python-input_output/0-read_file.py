68fdc788e2c7f92032cb65dc0104889aac9f0fa3
ZALOFARG <andre.work.go@gmail.com>
2023-12-12 21:09:35 -0800

    larger docstring

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-"""this module will handle I/O"""
+"""this module will handle I/O in terms of opening, writting and closing files"""
 
 
 def read_file(filename=""):
-    """func that reads a file"""
+    """func that reads a file
+    Args:
+    filename: object to be read
+
+    Returns:
+    None
+    """
     with open(filename, 'r', encoding="UTF8") as f:
         content = f.read()
         print(content.strip(), end='')
