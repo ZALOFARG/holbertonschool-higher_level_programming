9ad33b04de7976bd9ff9932c1ecde15256fbd70b
ZALOFARG <andre.work.go@gmail.com>
2023-12-23 18:05:43 -0800

    fulfilling the requirements

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- CREATES A NEW TABLE AND ADD MULTIPLE ROWS
-CREATE IF NOT EXISTS TABLE second_table(id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table(id, name, score)
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score)
 	VALUES 	(1, "John", 10),
 		(2, "Alex", 3),
 		(3, "Bob", 14),
