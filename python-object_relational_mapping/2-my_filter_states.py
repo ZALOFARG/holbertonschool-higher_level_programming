c4e79cab10a0cf1aee5764f8a28462ce45de2f03
ZALOFARG <andre.work.go@gmail.com>
2023-12-29 14:50:36 -0800

    1st commit t2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""this module will displays all values in the states table of hbtn_0e_0_usa where name matches the argument"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(sys.argv[4].strip("'")))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
