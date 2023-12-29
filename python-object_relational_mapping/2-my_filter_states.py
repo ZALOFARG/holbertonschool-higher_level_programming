350dac8a9ff2a3d6c1e731587becddb501085e5b
ZALOFARG <andre.work.go@gmail.com>
2023-12-29 14:56:03 -0800

    pycode compliant

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""this module will displays all values in the states table of hbtn_0e_0_usa where name matches the argument"""
+"""this module will displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument
+"""
 
 import MySQLdb
 import sys
@@ -7,7 +9,9 @@ import sys
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(sys.argv[4].strip("'")))
+    cur.execute("""SELECT * FROM states
+            WHERE name LIKE '{}'
+            ORDER BY id ASC""".format(sys.argv[4].strip("'")))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
