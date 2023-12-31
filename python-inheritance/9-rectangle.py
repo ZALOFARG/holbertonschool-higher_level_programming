cee3b1eea51bfc80e7bd86e49f23d9d0f685791c
ZALOFARG <andre.work.go@gmail.com>
2023-12-30 20:36:17 -0800

    1st commit t9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""this module will define a class then work with it"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """working with a class"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Return the area"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns a informal string repr of the instance"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
