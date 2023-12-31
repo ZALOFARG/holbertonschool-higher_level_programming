18ffb084b8e46e2b4980098784ec00fbb8f34f29
ZALOFARG <andre.work.go@gmail.com>
2023-12-31 05:10:21 -0800

    PEP8 comp

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -3,12 +3,13 @@
 """Call the json module"""
 import json
 
-def save_to_json_file(my_obj, filename):
-        """Returns an object represented by a JSON string.
 
-            Args:
-                my_obj (obj): Object to parse to JSON.
-                filename (file): File to put the parsed object
-        """
-        with open(filename, 'w', encoding="utf-8") as f:
-            f.write(json.dumps(my_obj))
+def save_to_json_file(my_obj, filename):
+    """Returns an object represented by a JSON string.
+
+    Args:
+        my_obj (obj): Object to parse to JSON.
+        filename (file): File to put the parsed object
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(json.dumps(my_obj))
