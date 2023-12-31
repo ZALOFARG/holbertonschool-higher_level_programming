1d58d445d8d34b656d8c0c7b988bbd903433a058
ZALOFARG <andre.work.go@gmail.com>
2023-12-30 20:30:00 -0800

    another try

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""this module will define a class then work with it"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """working with a class"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
