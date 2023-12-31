2d038dfadbf082303105532730a558225b93af04
ZALOFARG <andre.work.go@gmail.com>
2023-12-31 05:31:41 -0800

    1st commit t12

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Write a script that changes state with id 2 to New Mexico
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State).filter(State.id == 2).first()
+    query.name = "New Mexico"
+    session.commit()
+    session.close()
