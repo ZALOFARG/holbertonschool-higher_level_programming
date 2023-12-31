7129ad4d01cf5a0cbbba54fe8454f239e6c9c9de
ZALOFARG <andre.work.go@gmail.com>
2023-12-30 20:56:51 -0800

    fufilling requirements

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -10,9 +10,8 @@ def read_file(filename=""):
     Returns:
     None
     """
-    with open(filename, 'r', encoding="UTF8") as f:
-        content = f.read()
-        print(content.strip(), end='')
+    with open(filename, encoding="UTF8") as f:
+        print(f.read(), end="")
 
 
 if __name__ == '__main__':
