4bbaf8b5c42a8411f9334c82c991dc81e1e2f9f0
ZALOFARG <andre.work.go@gmail.com>
2023-12-31 05:11:32 -0800

    PEP8 compliant

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -3,11 +3,12 @@
 """Call the json module"""
 import json
 
-def load_from_json_file(filename):
-        """Creates an Object from a JSON file.
 
-            Args:
-                filename (file): File in JSON representation.
-        """
-        with open(filename, encoding="utf-8") as f:
-            return json.loads(f.read())
+def load_from_json_file(filename):
+    """Creates an Object from a JSON file.
+
+     Args:
+        filename (file): File in JSON representation.
+    """
+    with open(filename, encoding="utf-8") as f:
+        return json.loads(f.read())
