fee9e1bc53171ed2c3cefceaad847983d34ca1d0
ZALOFARG <andre.work.go@gmail.com>
2023-12-31 04:28:22 -0800

    1st commit t5

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Call the json module"""
+import json
+
+def save_to_json_file(my_obj, filename):
+        """Returns an object represented by a JSON string.
+
+            Args:
+                my_obj (obj): Object to parse to JSON.
+                filename (file): File to put the parsed object
+        """
+        with open(filename, 'w', encoding="utf-8") as f:
+            f.write(json.dumps(my_obj))
