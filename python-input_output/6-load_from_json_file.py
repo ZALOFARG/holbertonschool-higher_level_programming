7cb51fe2aa2d4a6c2116fbcf0089a5bd3d659dfb
ZALOFARG <andre.work.go@gmail.com>
2023-12-31 04:33:08 -0800

    1st commit t6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Call the json module"""
+import json
+
+def load_from_json_file(filename):
+        """Creates an Object from a JSON file.
+
+            Args:
+                filename (file): File in JSON representation.
+        """
+        with open(filename, encoding="utf-8") as f:
+            return json.loads(f.read())
