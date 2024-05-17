60d4de7dc450ca198ef70e20c02f3b0ebaef431a
ZALOFARG <andre.work.go@gmail.com>
2024-05-17 12:17:36 -0500

    fix: erase the wrong quotation marks

--- javascript-web_jquery/7-script.js
+++ javascript-web_jquery/7-script.js
@@ -1,4 +1,4 @@
-$('document').ready(() => {
+$(document).ready(() => {
   $.get('https://swapi-api.hbtn.io/api/people/5/?format=json', (data, textStatus) => {
     $('DIV#character').text(data.name);
   });
